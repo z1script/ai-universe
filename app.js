c665d7554709b892bc483185d84be2e4352d19aa
Zihadul Islam <zynscriptofficial@gmail.com>
2023-03-31 22:32:44 +0600

    data loaded, fetch api

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("app.js connected");
+const loadAiTools = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => displayAiTools(data.data.tools));
+};
+
+const displayAiTools = (tools) => {
+  console.log(tools);
+};
+
+loadAiTools();
